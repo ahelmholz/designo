d73a79975ea16d07088ef1a36ee440cf66dd5d1c
Austin Helmholz <austin.helmholz@gmail.com>
2023-03-10 12:43:51 -0600

    add: company page coming soon

---
+++ pages/company.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {Heading} from "@chakra-ui/react";
+
+const CompanyPage = () => {
+    return (
+        <div>
+        <Heading textAlign="center"> Company Page Coming Soon</Heading>
+        </div>
+    )
+}
+
+export default CompanyPage;
